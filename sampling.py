34a5564e10262a35569ab0dd3f1992d3bd29f86f
hassnov <hassnov@live.com>
2016-07-17 20:08:58 +0200

    add random sampling for testing

--- sampling.py
+++ sampling.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class SampleAlgorithm(Enum):
     Uniform = 1
+    Random = 2
 
 class Sampler:
 
@@ -10,6 +11,12 @@ class Sampler:
     def __init__(self):
         print ''
 
+    @staticmethod
+    def sample_random(pc, num_point):
+        indices = np.random.permutation(pc.shape[0])[0:num_point, ...]
+        sampled_pc = pc[indices]
+        return sampled_pc
+    
     @staticmethod
     def sample_uniform(pc, sample_step):
         indices = np.arange(0, pc.shape[0]-1, int(sample_step))
@@ -20,5 +27,8 @@ class Sampler:
     def sample(pc, rel_Distance, min_num_point, sampling_algorithm=SampleAlgorithm.Uniform):
         if sampling_algorithm == SampleAlgorithm.Uniform:
             return Sampler.sample_uniform(pc, int(pc.shape[0] / min_num_point))
+        else:
+            if sampling_algorithm == SampleAlgorithm.Random:
+                return Sampler.sample_uniform(pc, min_num_point)
         return -1
 
