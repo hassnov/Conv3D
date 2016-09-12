40415efaaa9826270246959a16adf71d017afba3
hassnov <hassnov@live.com>
2016-09-12 16:46:36 +0200

    Plot TSDF

--- plotutils.py
+++ plotutils.py
@@ -25,31 +25,72 @@ def plot_patch_3D(patch, name='Patch'):
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
+    #TODO: general size
+    ax.set_xlim([0,31])
+    ax.set_ylim([0,31])
+    ax.set_zlim([0,31])
     
     return fig
 
+def plot_patch_TSDF(patch, cutoff=0.1, name='Patch'):
+    x = []
+    y = []
+    z = []
+    s = []
+    sizex = patch.shape[0]
+    sizey = patch.shape[1]
+    sizez = patch.shape[2]
+    print 'size: ', patch.shape
+    for xi in np.arange(0, sizex):
+        for yi in np.arange(0, sizey):
+            for zi in np.arange(0, sizez):
+                if patch[xi, yi, zi] < 0.2:
+                    x.append(xi/float(1000))
+                    y.append(yi/float(1000))
+                    z.append(zi/float(1000))
+                    s.append(patch[xi, yi, zi])
+    
+    #s = np.reshape(patch, [sizex*sizey*sizez]) / np.max(patch)
+    #print patch
+    print 'smax: ', np.max(s)
+    return mlab.points3d(x, y, z, s, colormap='Spectral', scale_factor=0.004)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter3D(x, y, z, c=np.reshape(patch, [sizex*sizey*sizez]))
+    plt.title(name)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.set_xlim([0,sizex-1])
+    ax.set_ylim([0,sizey-1])
+    ax.set_zlim([0,sizez-1])
+    
+    return fig
 
-def show_pc(pc, points_in_pc=[], mode='point'):
+def show_pc(pc, points_in_pc=[], mode='point', scale_factor = 0.001):
     pts = mlab.points3d(pc[:, 0], pc[:, 1], pc[:, 2], color=(1, 1, 1), mode='point')
     
     if len(points_in_pc) > 0:
         pts = mlab.points3d(points_in_pc[:, 0], points_in_pc[:, 1], points_in_pc[:, 2],np.full(points_in_pc[:, 2].shape, 1, dtype='i8'),
-                      color=(0, 0, 0), colormap='Spectral', mode=mode, scale_factor=0.001)
+                      color=(0, 0, 0), colormap='Spectral', mode=mode, scale_factor=scale_factor)
         pts.glyph.glyph.clamping = False
     
 
 
 def show_matches(pc1, pc2, samples1, samples2, matches, tube_radius=0.0003, N=50):
-    axis = 2
-    pc1[:, axis] -= 0.1
-    samples1[:, axis] -= 0.1
-    pc2[:, axis] += 0.1
-    samples2[:, axis] += 0.1
+    axis = 0
+    diff = 1
+    bbx = utils.compute_bounding_box_std(pc1)
+    pc1[:, axis] -= (bbx[axis][1] - bbx[axis][0])*1.5
+    samples1[:, axis] -= (bbx[axis][1] - bbx[axis][0])*1.5
+    pc2[:, axis] += (bbx[axis][1] - bbx[axis][0])*1.5
+    samples2[:, axis] += (bbx[axis][1] - bbx[axis][0])*1.5
     show_pc(pc1, pc2)
     assert(samples1.shape[0] == samples2.shape[0] == matches.shape[0])
     if matches.shape[0] > N:
-        samples11 = samples1[matches[0:50,0].astype(dtype=np.int16)]
-        samples22 = samples2[matches[0:50,1].astype(dtype=np.int16)]
+        samples11 = samples1[matches[0:N,0].astype(dtype=np.int16)]
+        samples22 = samples2[matches[0:N,1].astype(dtype=np.int16)]
     else:
         samples11 = samples1[matches[:,0].astype(dtype=np.int16)]
         samples22 = samples2[matches[:,1].astype(dtype=np.int16)]
