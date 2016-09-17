a9a6c10372e597dffa34072d3383dad115649ac7
hassnov <hassnov@live.com>
2016-09-17 14:55:43 +0200

    add sampling by iss detector (needs pcl or sampled files)

--- sampling.py
+++ sampling.py
@@ -1,9 +1,13 @@
 import numpy as np
 from enum import Enum
+import subprocess
+import os.path
+from plyfile import PlyData
 
 class SampleAlgorithm(Enum):
     Uniform = 1
     Random = 2
+    ISS_Detector = 3
 
 class Sampler:
 
@@ -24,7 +28,32 @@ class Sampler:
         return sampled_pc, indices
 
     @staticmethod
-    def sample(pc, rel_Distance, min_num_point, sampling_algorithm=SampleAlgorithm.Uniform):
+    def sample_ISS(min_num_point , file_name):
+        root, ext = os.path.splitext(file_name)
+        in_file = root + ".ply"
+        out_file = root + "_iss.ply"
+        if (not os.path.isfile(out_file)):
+            args = ["points/iss_detect", in_file, out_file]
+            popen = subprocess.Popen(args, stdout=subprocess.PIPE)
+            popen.wait()
+            output = popen.stdout.read()
+            print output
+        ply = PlyData.read(out_file)
+        vertex = ply['vertex']
+        (x, y, z) = (vertex[t] for t in ('x', 'y', 'z'))
+        pc_iss = np.asarray(zip(x.ravel(), y.ravel(), z.ravel()))
+        sample_step = int(pc_iss.shape[0] / min_num_point)
+        return Sampler.sample_uniform(pc_iss, sample_step)[0], -1
+        
+        #indices = np.arange(0, pc.shape[0]-1, int(sample_step))
+        #sampled_pc = pc[indices]
+        #return sampled_pc, indices
+
+
+    @staticmethod
+    def sample(pc, rel_Distance, min_num_point, file_name="points/points.npy",
+               pose = np.identity(4),
+               sampling_algorithm=SampleAlgorithm.Uniform):
         if sampling_algorithm == SampleAlgorithm.Uniform:
             return Sampler.sample_uniform(pc, int(pc.shape[0] / min_num_point))
         else:
