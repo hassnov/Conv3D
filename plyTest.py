b0429f9d1e62f7d656dc9e0a40ae0df5f872e6bb
hassnov <hassnov@live.com>
2016-09-22 19:58:43 +0200

    testing

--- plyTest.py
+++ plyTest.py
@@ -339,18 +339,18 @@ def main_sample():
 import os
 
 def main_patch():
-	dir1 = os.path.dirname(os.path.realpath(__file__))
-	sample_points = utils.read_ply(os.path.join(dir1, 'plytest/bun_zipper_sampled.ply'))
-	pc = utils.read_ply(os.path.join(dir1, 'plytest/bun_zipper.ply'))
-	num_rotations = 20
-	patch_dim = 32
-	relSampling = 0.05
-	relRadius = 0.1
-	pc_diameter = utils.get_pc_diameter(pc)
-	radius = pc_diameter*relRadius
-	relL = 0.07
-	l = relL*pc_diameter
-	extract_patches_vox(pc, sample_points, l=l, num_rotations=num_rotations, patch_dim=patch_dim)
+    dir1 = os.path.dirname(os.path.realpath(__file__))
+    sample_points = utils.read_ply(os.path.join(dir1, 'plytest/bun_zipper_sampled.ply'))
+    pc = utils.read_ply(os.path.join(dir1, 'plytest/bun_zipper.ply'))
+    num_rotations = 20
+    patch_dim = 32
+    relSampling = 0.05
+    relRadius = 0.1
+    pc_diameter = utils.get_pc_diameter(pc)
+    radius = pc_diameter*relRadius
+    relL = 0.07
+    l = relL*pc_diameter
+    extract_patches_vox(pc, sample_points, l=l, num_rotations=num_rotations, patch_dim=patch_dim)
 
 
 def main_noise():
@@ -364,4 +364,10 @@ def main_noise():
     plotutils.show_pc(noisy)
     mlab.show()
 
-main_patch()
+def main_show_ply():
+    pc = utils.read_ply('/home/hasan/Downloads/3D models/Stanford/armadillo/Armadillo_vres2_small_scaled_0.ply')
+    plotutils.show_pc(pc)
+    mlab.show()
+    
+    
+main_show_ply()
