285a4fd2e6351826eda2ae5b7a6a5b01bb3f6074
hassnov <mahmoud.mohamed.bahaa@gmail.com>
2016-10-22 12:25:40 +0200

    change params

--- dataset_creator.py
+++ dataset_creator.py
@@ -5,6 +5,7 @@ import numpy as np
 from sampling import SampleAlgorithm
 import time
 import cluster_points
+import cluster_points_fpfh
 
 patch_dim = 32
          
@@ -13,29 +14,29 @@ def main():
     #num_rotations = 40
     #BATCH_SIZE = 2
     patch_dim = 32
-    files_file = os.path.join(dir1, 'files_file3.txt')
+    files_file = os.path.join(dir1, 'files_file4.txt')
     start_time = time.time()
     
     
-    reader = cluster_points.ClusterPoints()
+    reader = cluster_points_fpfh.ClusterPoints()
     #reader = PlyReader3.PlyReader()
     reader.create_reader(files_file, 0,
                           add_noise=False, noise_std=0.1, sampling_algorithm=SampleAlgorithm.ISS_Detector,
-                          num_classes=1100, relL=0.07, patch_dim=patch_dim,
+                          num_classes=-1, relL=0.07, patch_dim=patch_dim,
                           use_normals_pc=False, use_point_as_mean=False, flip_view_point=False)
     
-    reader.create_dataset(dir_temp='temp_1100/')
+    reader.create_dataset(dir_temp='temp_fpfh/')
     
     
     return 0
 
     
 
-def load(dir_temp='temp/'):
-    reader = cluster_points.ClusterPoints()
-    reader.load_dataset(dir_temp='temp/')
+def load(dir_temp='temp/', bandwidth=0.00001):
+    reader = cluster_points_fpfh.ClusterPoints()
+    reader.load_dataset(dir_temp=dir_temp, bandwidth=bandwidth)
     
     
-#load(dir_temp='temp1/', bandwidth=0.0001)
-main()
+load(dir_temp='temp_fpfh/', bandwidth=0.01)
+#main()
 
